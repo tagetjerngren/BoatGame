8dbb97fefc3191eac316aca63aef12ba815cab49
Tage <tagetjerngren@gmail.com>
2026-04-29 22:32:34 +0200

    Feat: Add MultiRaycast function that returns an array of objects

--- source/scripts/Misc/utils.lua
+++ source/scripts/Misc/utils.lua
@@ -16,6 +16,7 @@ end
 local point_new <const> = playdate.geometry.point.new
 local QuerySpriteInfoAlongLine <const> = playdate.graphics.sprite.querySpriteInfoAlongLine
 
+-- NOTE: ignoreClassesList is an array of strings, not so good
 function Raycast(sourceX, sourceY, directionX, directionY, ignoreSpritesList, ignoreClassesList)
 	local source = point_new(sourceX, sourceY)
 	local collisions, _ = QuerySpriteInfoAlongLine(sourceX, sourceY, sourceX + directionX, sourceY + directionY)
@@ -49,4 +50,36 @@ function Raycast(sourceX, sourceY, directionX, directionY, ignoreSpritesList, ig
 		end
 	end
 	return closestCollision, collisionPoint
+end
+
+-- NOTE: ignoreClassesList is an array of strings, not so good
+function MultiRaycast(sourceX, sourceY, directionX, directionY, ignoreSpritesList, ignoreClassesList)
+	local source = point_new(sourceX, sourceY)
+	local collisions, _ = QuerySpriteInfoAlongLine(sourceX, sourceY, sourceX + directionX, sourceY + directionY)
+	local resultCollisions = {}
+	for _, collision in ipairs(collisions) do
+		local ignored = false
+		if ignoreSpritesList then
+			for _, ignoreSprite in ipairs(ignoreSpritesList) do
+				if collision.sprite == ignoreSprite then
+					ignored = true
+					break
+				end
+			end
+		end
+
+		if not ignored and ignoreClassesList then
+			for _, ignoreClass in ipairs(ignoreClassesList) do
+				if collision.sprite.className == ignoreClass then
+					ignored = true
+					break
+				end
+			end
+		end
+
+		if not ignored then
+			table.insert(resultCollisions, collision)
+		end
+	end
+	return resultCollisions
 end
