86dc4b7a49c8df70b253cbbb4a8415ea8fca162c
Tage <tagetjerngren@gmail.com>
2026-04-30 15:34:51 +0200

    Fix: Stop using nineslice to improve performance

--- source/scripts/Scenes/ability_menu.lua
+++ source/scripts/Scenes/ability_menu.lua
@@ -5,6 +5,8 @@ import "CoreLibs/nineslice"
 
 class('AbilityMenu').extends(gfx.sprite)
 
+local BackgroundNineSlice = gfx.nineSlice.new("images/WallResizable", 5, 5, 6, 6)
+
 function AbilityMenu:init(gameManager)
 	self.gameManager = gameManager
 	self.gameManager.player.bActive = false
@@ -21,7 +23,7 @@ function AbilityMenu:init(gameManager)
 	self.grid:setNumberOfRows(2)
 	self.grid:setCellPadding(2, 2, 2, 2)
 	-- self.grid.backgroundImage = gfx.nineSlice.new("images/gridBackground", 8, 8, 47, 47)
-	self.grid.backgroundImage = gfx.nineSlice.new("images/WallResizable", 5, 5, 6, 6)
+	self.grid.backgroundImage = BackgroundNineSlice
 	self.grid:setContentInset(5, 5, 5, 5)
 	self.abilities = self.gameManager.PlayerData.abilities
 	local icons = {}
@@ -35,31 +37,26 @@ function AbilityMenu:init(gameManager)
 				local row = math.floor(i / 4) + 1
 				local column = (i % 4)
 				self.grid:setSelection(1, row, column)
-				print("Row: "..row.."\nColumn: "..column)
+				-- print("Row: "..row.."\nColumn: "..column)
 				-- local a = (row - 1) * 4 + column
 				break
 			end
 		end
 	end
 
-	-- local ns = gfx.nineSlice.new("images/WallResizable", 5, 5, 6, 6)
-	-- local nsBlank = gfx.nineSlice.new("images/OneWayDoor", 5, 5, 22, 22)
-	local ns = gfx.nineSlice.new("images/CollectionCellSelected", 12, 12, 24, 24)
-	local nsBlank = gfx.nineSlice.new("images/CollectionCell", 12, 12, 24, 24)
+	local selectedCellImage = gfx.image.new("images/AbilityCellSelected")
+	local unselectedCellImage = gfx.image.new("images/AbilityCell")
 	function self.grid:drawCell(section, row, column, selected, x, y, width, height)
 		gfx.setColor(gfx.kColorBlack)
 		if selected then
-			ns:drawInRect(x, y, width, height)
-			-- gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+			selectedCellImage:draw(x, y)
 			gfx.setImageDrawMode(gfx.kDrawModeInverted)
 			gfx.setColor(gfx.kColorWhite)
 			icons[(row - 1) * 4 + column]:draw(x + 6, y + 6)
-			-- gfx.drawTextInRect("*"..options[row].."*", x, y + (height/2) - 5 + 3 * math.sin(7 * pd.getElapsedTime()), width, height, nil, nil, kTextAlignment.center)
 		else
 			gfx.setImageDrawMode(gfx.kDrawModeCopy)
-			nsBlank:drawInRect(x, y, width, height)
+			unselectedCellImage:draw(x, y)
 			icons[(row - 1) * 4 + column]:draw(x + 6, y + 6)
-			-- gfx.drawTextInRect(options[row], x, y + (height/2) - 5, width, height, nil, nil, kTextAlignment.center)
 		end
 	end
 	self:add()
@@ -68,6 +65,8 @@ end
 
 local movingSound = pd.sound.sampleplayer.new("sounds/ChangingSelection")
 local decisionSound = pd.sound.sampleplayer.new("sounds/SelectionMade")
+local GridWidth = 48 * 4 + 10
+local GridHeight = 48 * 2 + 10
 
 function AbilityMenu:update()
 	if self.bActive then
@@ -85,7 +84,6 @@ function AbilityMenu:update()
 			movingSound:play()
 		elseif pd.buttonJustReleased(pd.kButtonA) then
 			local _, row, column = self.grid:getSelection()
-			print("Row: "..row.."\nColumn: "..column)
 			if self.abilities[(row - 1) * 4 + column].func then
 				self.gameManager.player.bActive = true
 				self.gameManager.PlayerData.activeAbility = self.abilities[(row - 1) * 4 + column]
@@ -99,18 +97,9 @@ function AbilityMenu:update()
 		end
 	end
 
-
-	-- if #self.options == 2 then
-	-- 	self.grid:drawInRect(0, 0, 235 - 60, 240 - 60)
-	-- else
-	-- local gridImage = gfx.image.new(385, 178)
 	gfx.lockFocus(self:getImage())
-	local GridWidth = 48 * 4 + 10
-	local GridHeight = 48 * 2 + 10
 	self.grid:drawInRect((GridWidth / 2), (GridHeight / 2) + 30, GridWidth, GridHeight)
 	gfx.unlockFocus()
-	-- gridImage:drawIgnoringOffset(32, 48)
-	-- end
 
 	gfx.setColor(gfx.kColorBlack)
 end
